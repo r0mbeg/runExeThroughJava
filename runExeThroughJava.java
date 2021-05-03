ff2dc630d9cc203053df2e9c6db335ff5c57e569
Roman <37178784+r0mbeg@users.noreply.github.com>
2021-05-04 00:13:51 +0300

    Update runExeThroughJava.java

--- runExeThroughJava.java
+++ runExeThroughJava.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -7,56 +8,66 @@ import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		int flag = 0;
-		Scanner in = new Scanner(System.in);
-		System.out.print("Enter the path to Python -> ");
-		String pyPath = in.nextLine();
-		//C:\Program Files\Python\Python37\Python.exe 
-		
-		ProcessBuilder builder = new ProcessBuilder(pyPath, "-m", "timeit", "-r", "10");
-		
-		builder.redirectErrorStream(true);
-		
-	    Process process = builder.start();
-	    MyThread thread = new MyThread();
-	    thread.flag = 1;
-	    thread.start();
-	   	    
-	    //вывод результата
-	    try ( BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream())) ) {
-	        String line;
-	        while ((line = br.readLine()) != null) {
-	            System.out.println(line);
-	        }
-	    }
-	    
-	    process.waitFor();
-	    thread.flag = 2;
-	    
+
+		try(Scanner sc = new Scanner(System.in)){
+
+			System.out.print("Enter the path to Python -> ");
+			String pyPath = sc.nextLine();//C:\Program Files\Python\Python37\Python.exe
+
+			ProcessBuilder builder = new ProcessBuilder(pyPath, "-m", "timeit", "-r", "10");
+			builder.redirectErrorStream(true);
+
+			Process process = builder.start();
+			MyThread thread = new MyThread();
+			thread.flag = 1;
+			thread.start();
+
+
+			try ( BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream())) ) {
+				String line;
+				while ((line = br.readLine()) != null) {
+					System.out.println(line);
+
+				}
+			}
+
+			process.waitFor();
+			thread.flag = 2;
+
+
+
+		} catch(FileNotFoundException e) {
+			System.out.println("File not found");
+
+		} catch(IOException ioe) {
+			System.out.println("File reading error");
+
+		} 
+
 	}
 }
 
 class MyThread extends Thread {
-	
+	//readme Р¤Р°Р№Р»С‹, С‚РµСЃС‚С‹
 	public int flag;
 	public long start;  
 	MyThread(){
 		start = System.currentTimeMillis();
 	}
-	
+
 	public void run() {
 		try {
 			while (flag == 1) {
-			
-			long finish = System.currentTimeMillis();
-			long elapsed = finish - start;
-			System.out.println("Прошло времени, сек: " + elapsed/1000);
-			Thread.sleep(1000);
+
+				long finish = System.currentTimeMillis();
+				long elapsed = finish - start;
+				System.out.println("РџСЂРѕС€Р»Рѕ РІСЂРµРјРµРЅРё, СЃРµРє: " + elapsed/1000);
+				Thread.sleep(1000);
 			}
-			
+
 		} catch(InterruptedException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 }
